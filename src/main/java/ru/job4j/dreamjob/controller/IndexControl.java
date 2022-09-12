56d5252787800ae047b25858a51306a70d4d10d7
ProstoARS <arsudakov@gmail.com>
2022-09-12 17:57:29 +0300

    1. Что такое клиент - серверное приложение. [#504837]

---
+++ src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
@@ -0,0 +1,13 @@
+package ru.job4j.dreamjob.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexControl {
+
+    @GetMapping("/index")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}

a236ed9666993f0a074ba0d14e77ded4e186ad1f
ProstoARS <arsudakov@gmail.com>
2022-09-19 13:02:34 +0300

    4. Thymeleaf, Циклы. [#504841 #344899]
    4. Thymeleaf, Циклы. [#504841 #344899]

---
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -0,0 +1,18 @@
+package ru.job4j.dreamjob.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.store.PostStore;
+
+@Controller
+public class PostController {
+
+    private final PostStore store = PostStore.instOf();
+
+    @GetMapping("/posts")
+    public String posts(Model model) {
+        model.addAttribute("posts", store.findAll());
+        return "posts";
+    }
+}
---
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -0,0 +1,27 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Post;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PostStore {
+
+    private static final PostStore INST = new PostStore();
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private PostStore() {
+        posts.put(1, new Post(1, "Junior Java Job"));
+        posts.put(2, new Post(2, "Middle Java Job"));
+        posts.put(3, new Post(3, "Senior Java Job"));
+    }
+
+    public static PostStore instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
